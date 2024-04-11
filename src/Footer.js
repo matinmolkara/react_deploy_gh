1d9b8ec358e6f09fdf4d74350a56b8485dcfe054
matin <97048003+matinmolkara@users.noreply.github.com>
2024-04-11 20:15:19 +0330

    firts-comment

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+const Footer = ({length}) => {
+   
+  return (
+   <footer>
+        <h1>
+            {length} List {length === 1 ? "item" : "items"}
+        </h1>
+   </footer>
+  )
+}
+
+export default Footer
